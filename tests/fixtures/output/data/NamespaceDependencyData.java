b5738c91dd4793ceb6243c2b284d02cdbe1e4f7e
Guilherme Lacerda <guilhermeslacerda@gmail.com>
2021-08-26 14:30:46 -0300

    removing dead code

--- tests/fixtures/output/data/NamespaceDependencyData.java
+++ tests/fixtures/output/data/NamespaceDependencyData.java
@@ -1,6 +1,6 @@
 package fixtures.output.data;
 
-public class NamespaceDependencyData { //implements Comparable<NamespaceDependencyData> {
+public class NamespaceDependencyData { 
 	private String source;
 	private String target;
 	
@@ -16,13 +16,4 @@ public class NamespaceDependencyData { //implements Comparable<NamespaceDependen
 	public String getTarget() {
 		return target;
 	}
-
-//	@Override
-//	public int compareTo(NamespaceDependencyData other) {
-//		if (this.getTarget().compareToIgnoreCase(other.getTarget()) > 1)
-//			return 1;
-//		if (this.getTarget().compareToIgnoreCase(other.getTarget()) < 0)
-//			return -1;
-//		return 0;
-//	}
 }
